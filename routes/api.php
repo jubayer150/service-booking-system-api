887b32ca41cdc235b4723bd366a1162ff33c0f0a
Jubayer Rahman <jubayerr.150@gmail.com>
2025-08-28 00:18:05 +0600

    add: install Sanctum for token auth

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Support\Facades\Route;
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    
+});
+
+require __DIR__ . '/auth_api.php';
